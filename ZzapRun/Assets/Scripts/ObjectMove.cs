a72ef14c7e718b81de242406da67a7291dd1a77e
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2021-09-28 15:49:23 +0900

    아이템 이동

--- ZzapRun/Assets/Scripts/ObjectMove.cs
+++ ZzapRun/Assets/Scripts/ObjectMove.cs
@@ -5,31 +5,10 @@ using UnityEngine;
 public class ObjectMove : MonoBehaviour
 {
     Rigidbody2D rigid;
-    GameObject item;
 
-    [SerializeField]
-    float moveForce;
-    [SerializeField]
-    float moveTime;
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
-        item = GetComponent<GameObject>();
-    }
-
-    void Start()
-    {
-        move();
-    }
-
-    void Update()
-    {
-        
-    }
-
-    public void move()
-    {
-        transform.position += Vector3.left;
-        Invoke("move", moveTime);
+        rigid.velocity = Vector2.left * 3;
     }
 }
